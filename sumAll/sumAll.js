da68a54cc437147c236d32b3cc10447d554ff40e
Michael <mdong185@gmail.com>
2019-04-11 15:35:50 -0400

    solved

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
 const sumAll = function() {
+	let ans = 0;
+	
+	if (arguments[0] < 0 || Number.isInteger(arguments[0]) == false || arguments[1] < 0 || Number.isInteger(arguments[1]) == false) {
+		ans = "ERROR"
+	} else {
+		for (i= Math.min(arguments[0],arguments[1]); i <= Math.max(arguments[0],arguments[1]); i++){
+			ans += i;
+		}
+	}
 
+	return ans;
 }
 
 module.exports = sumAll
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f_temp) {
+	let ans = (f_temp - 32) * (5/9)
+	return Math.round(ans * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(c_temp) {
+	let ans = c_temp * (9/5) + 32
+	return Math.round(ans * 10) / 10;
 }
 
 module.exports = {
